fdfc5da08a2f57e16ce1401189f2b747d6ed4ac3
fedjes <salvados78@mail.ru>
2021-05-11 17:23:16 +0300

    added

---
+++ app.js
@@ -0,0 +1,22 @@
+function sum (numb, numb2) {
+let num = document.getElementById("numb").value;
+let num2 = document.getElementById("numb2").value;
+document.getElementById("result").innerHTML = Number(num) + Number(num2);
+}
+function sub (numb, numb2) {
+    let num = document.getElementById("numb").value;
+    let num2 = document.getElementById("numb2").value;
+    document.getElementById("result").innerHTML = Number(num) - Number(num2);
+}
+
+function mul (numb, numb2) {
+    let num = document.getElementById("numb").value;
+    let num2 = document.getElementById("numb2").value;
+    document.getElementById("result").innerHTML = Number(num) * Number(num2);
+}
+
+function div (numb, numb2) {
+    let num = document.getElementById("numb").value;
+    let num2 = document.getElementById("numb2").value;
+    document.getElementById("result").innerHTML = Number(num) / Number(num2); 
+}
